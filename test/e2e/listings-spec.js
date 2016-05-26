6241c93a5f04d066a63fb1f52a595d2728a9c162
Anne <annemarie.k89@gmail.com>
2016-05-26 14:55:45 +0100

    tests thats programmes starting with "b" are being displayed when clicking "b" in the navigation

--- test/e2e/listings-spec.js
+++ test/e2e/listings-spec.js
@@ -1,9 +1,17 @@
 describe("has listings from the bbc on the page", function() {
-  it("should list the first 20 results for 'a'", function() {
+  it("should list the first 20 results for 'a' on page load", function() {
     browser.get("/");
 
     var programmes = element.all(by.repeater("programme in ctrl.programmes"));
     expect(programmes.count()).toEqual(20);
     expect(programmes.get(0).getText()).toEqual("Abadas");
+  });
+
+  it("should get programmes of 'b' when clicking 'b'", function() {
+    browser.get("/");
+    $("#letter-b").click();
+    var programmes = element.all(by.repeater("programme in ctrl.programmes"));
+    expect(programmes.count()).toEqual(20);
+    expect(programmes.get(0).getText()).toEqual("Babi Del");
   });
 });
