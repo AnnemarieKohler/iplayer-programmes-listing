5091b100638f06144886522f44a19a0354f321bb
Anne <annemarie.k89@gmail.com>
2016-05-26 18:24:01 +0100

    added param function which gets the user input for the letter and sends it to the get request

--- app/private/routes/index.js
+++ app/private/routes/index.js
@@ -8,11 +8,17 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express"});
 });
 
-router.get("/listings", function(req, res, next) {
+var saveLetter;
+
+router.param("letter", function(req, res, next, letter) {
+  saveLetter = letter;
+  next();
+});
+
+router.get("/listings/:letter", function(req, res, next) {
   var baseUrl = "https://ibl.api.bbci.co.uk/ibl/v1/atoz/";
-  var letter = "a";
   var page = "1";
-  var bbcUrl = baseUrl + letter + "/programmes?page=" + page;
+  var bbcUrl = baseUrl + saveLetter + "/programmes?page=" + page;
 
   request(bbcUrl, function (error, response, body) {
     var programmesList = JSON.parse(body).atoz_programmes.elements;
