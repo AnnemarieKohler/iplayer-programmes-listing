a12945aead6c7ee704455096439fef96074adcf8
Anne <annemarie.k89@gmail.com>
2016-05-26 18:21:18 +0100

    added check for letter b and first example of letter b

--- test/unit/test-server.js
+++ test/unit/test-server.js
@@ -16,15 +16,28 @@ describe("Index page", function() {
     });
   });
 
-  it("Should load 'listings' page", function() {
+  it("Should load 'listings/a' page", function() {
     chai.request(server)
-    .get("/listings")
+    .get("listings/a")
     .end(function(err, res){
       res.should.have.status(200);
       res.should.be.json;
       res.body.should.be.a("array");
       res.body[0].should.have.property('title');
       res.body[0].title.should.equal('Abadas');
+      done();
+    });
+  });
+
+  it("Should load 'listings/b' page", function() {
+    chai.request(server)
+    .get("listings/b")
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a("array");
+      res.body[0].should.have.property('title');
+      res.body[0].title.should.equal('Babi Del');
       done();
     });
   });
