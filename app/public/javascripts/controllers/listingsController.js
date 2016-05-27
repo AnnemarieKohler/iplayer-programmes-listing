b03fe01a1d76a5d5e7578977c6692b0a500a132a
Anne <annemarie.k89@gmail.com>
2016-05-27 07:09:32 +0100

    #showProgrammes now takes page value and calls on private method to create an array for the number of Pages, which is based on the number of programmes.

--- app/public/javascripts/controllers/listingsController.js
+++ app/public/javascripts/controllers/listingsController.js
@@ -5,15 +5,30 @@ angular
 
       var self = this;
       self.programmes = [];
+      self.pages = [];
 
-      listingsService.getListings("a").then(function(response) {
-        self.programmes = response;
+      listingsService.getListings("a", "1").then(function(response) {
+        self.programmes = response.programmes;
+        var numberOfProgrammes = response.count;
+        _getPagesArray("a", numberOfProgrammes);
       });
 
-      self.showProgrammes = function(letter) {
-        listingsService.getListings(letter).then(function(response) {
-          self.programmes = response;
+      self.showProgrammes = function(letter, page) {
+        listingsService.getListings(letter, page).then(function(response) {
+          self.pages = [];
+          self.programmes = response.programmes;
+          var numberOfProgrammes = response.count;
+          _getPagesArray(letter, numberOfProgrammes);
         });
       };
+
+      function _getPagesArray(letter, numberOfProgrammes){
+        var numberOfPages = Math.ceil(numberOfProgrammes / 20);
+        for(var i = 1; i < (numberOfPages + 1); i++) {
+          self.pages.push({ character: letter, number: i.toString() });
+        }
+      }
+
+
 
     }]);
